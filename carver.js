f9bb955ba2529ad6bccc232acbebc803fe9f8835
Ilias Van Peer <ilias@idlemonkeys.net>
2012-10-10 21:01:59 +0200

    abstract convolutions, implement 3x3 max filter, display edge-detection result

--- carver.js
+++ carver.js
@@ -9,7 +9,7 @@ ImageData.prototype.getPixel = function(x, y) {
 	};
 }
 
-ImageData.prototype.setPixel = function(pixel, x, y) {
+ImageData.prototype.setPixel = function(x, y, pixel) {
 	if (typeof(pixel) == "number") {
 		pixel = {
 			r: pixel,
@@ -65,34 +65,51 @@ var Carver = {
 		return result;
 	},
 	sobel: function(bwmatrix) {
-		var total, x, y, dx, dy;
-	
-		var matrix = [
-			[0,  1, 0],
-			[1, -4, 1],
-			[0,  1, 0]
-		];
-		
+		return this.util.convolve(bwmatrix, [[0, 1, 0], [1, -4, 1], [0, 1, 0]]);
+	},
+	max: function(bwmatrix) {
+		var x, y, dx, dy, max, v;
 		var copy = new Uint8Matrix(bwmatrix);
-		
-		for (x = 1; x < bwmatrix.width-1; x++) {
-			for (y = 1; y < bwmatrix.height-1; y++) {
-				total = 0;
-				for (dx = 0; dx < 3; dx++) {
-					for (dy = 0; dy < 3; dy++) {
-						total += matrix[dx][dy] * bwmatrix.getCell(x + dx - 1, y + dy - 1);
+		for (x = 1; x < bwmatrix.width - 1; x++) {
+			for (y = 1; y < bwmatrix.height - 1; y++) {
+				max = 0;
+				for (dx = -1; dx <= 1; dx++) {
+					for (dy = -1; dy <= 1; dy++) {
+						v = bwmatrix.getCell(x+dx, y+dy);
+						max = max > v ? max : v;
 					}
 				}
-				copy.putCell(x, y, total);
+				copy.putCell(x, y, max);
 			}
 		}
 		
-		return copy;
+		return copy;	
 	},
-	
 	util: {
 		desaturate: function (r, g, b) {
 			return r * 0.21 + g * 0.72 + b * 0.07;
+		},
+		convolve: function(matrix, kernel) {
+			var total, x, y, dx, dy;
+			
+			var m = [].concat(kernel[0], kernel[1], kernel[2]);
+			var divisor = m.reduce(function(a, b) { return a+b;}) || 1;
+			
+			var copy = new Uint8Matrix(matrix);
+			
+			for (x = 1; x < matrix.width-1; x++) {
+				for (y = 1; y < matrix.height-1; y++) {
+					total = 0;
+					for (dx = 0; dx < 3; dx++) {
+						for (dy = 0; dy < 3; dy++) {
+							total += kernel[dx][dy] * matrix.getCell(x + dx - 1, y + dy - 1);
+						}
+					}
+					copy.putCell(x, y, total);
+				}
+			}
+			
+			return copy;
 		}
 	}
 };
@@ -108,8 +125,15 @@ img.onload = function() {
 	context.drawImage(img, 0, 0);
 	
 	var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-	var result = Carver.desaturate(imageData);
-	Carver.sobel(result);
+	var desat = Carver.desaturate(imageData);
+	var sobel = Carver.sobel(desat);
+	var max = Carver.max(sobel);
+	
+	for (var x = 0; x < imageData.width; x++) {
+		for (var y = 0; y < imageData.height; y++) {
+			imageData.setPixel(x, y, max.getCell(x, y));
+		}
+	}
 	
 	context.putImageData(imageData, 0, 0);
 }
